1db374795682904dc5093a47c7559a2c7dcc59ce
FROZEN-pro <sherzodmamatov10@gmail.com>
2026-06-24 18:07:42 +0500

    new update

--- js/config.js
+++ js/config.js
@@ -3,13 +3,7 @@ const config = {
     width: 1280,
     height: 720,
     backgroundColor: '#1a120b',
-    physics: {
-        default: 'arcade',
-        arcade: { gravity: { y: 0 }, debug: false }
-    },
+    physics: { default: 'arcade', arcade: { debug: false } },
     scene: [MenuScene, GuildHallScene],
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    }
+    scale: { mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH }
 };
